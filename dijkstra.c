5bfff74ec42e3b78a51b55d3f56f2ba2783e0c80
Matus Stas <matusstas.ms@gmail.com>
2019-02-10 13:37:31 +0100

    code refactoring

--- dijkstra.c
+++ dijkstra.c
@@ -3,12 +3,18 @@
 int main() {
 	int infinity = 1000000;
 	int n = 6;
-	int matrix_of_adjacency[6][6] = {0,7,9,0,0,14,7,0,10,15,0,0,9,10,0,0,0,2,0,15,0,0,6,0,0,0,0,6,0,9,14,0,2,0,9,0};
+	int matrix_of_adjacency[6][6] = {0,7,9,0,0,14,
+									 7,0,10,15,0,0,
+									 9,10,0,0,0,2,
+									 0,15,0,0,6,0,
+									 0,0,0,6,0,9,
+									 14,0,2,0,9,0};
 
 	int visited_vetrices[6] = {0,0,0,0,0,0};
 	int distance_between_vertices[6] = {infinity,infinity,infinity,infinity,infinity,infinity};
-
+ 	
 	int current;
+	printf("Select starting verticle from 0 to %d\n", n-1);
 	scanf("%d",&current);
 	int min = infinity;
 	int index = -1;
@@ -17,32 +23,20 @@ int main() {
 
 	for(int i =0; i < n; i++) {
 		for(int j = 0; j < n; j++) {
-			if(i == 0) {
-				if(matrix_of_adjacency[current][j] != 0) {
-					distance_between_vertices[j] = matrix_of_adjacency[current][j];
-
-					if(min > matrix_of_adjacency[current][j]) {
-						min = matrix_of_adjacency[current][j];
-						index = j;
-					}
+			if(visited_vetrices[j] != 1 && matrix_of_adjacency[current][j] != 0) {
+				if(distance_between_vertices[current] + matrix_of_adjacency[current][j] < distance_between_vertices[j])
+					distance_between_vertices[j] = distance_between_vertices[current] + matrix_of_adjacency[current][j];
 				}
-			}
-			else {
-				if(visited_vetrices[j] != 1 && matrix_of_adjacency[current][j] != 0) {
-					if(distance_between_vertices[current] + matrix_of_adjacency[current][j] < distance_between_vertices[j]) {
-						distance_between_vertices[j] = distance_between_vertices[current] + matrix_of_adjacency[current][j];
-					}
-				}
-				visited_vetrices[current] = 1;
-				if(visited_vetrices[j] != 1 && min > distance_between_vertices[j]) {
-					min = distance_between_vertices[j];
-					index = j;
-				}
+			visited_vetrices[current] = 1;
+			if(visited_vetrices[j] != 1 && min > distance_between_vertices[j]) {
+				min = distance_between_vertices[j];
+				index = j;
 			}
 		}
-		current = index;
-		min = infinity;
+	current = index;
+	min = infinity;
 	}
+	
 	for(int i = 0; i < n; i++)
 		printf("%d ", distance_between_vertices[i]);
 	putchar('\n');
